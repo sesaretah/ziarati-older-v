8fbbc5faaada077048cc446ffde388bd884043c1
Hossein Shafiei <h.shafiei@ut.ac.ir>
2019-02-26 10:34:10 +0330

    first commit

---
+++ www/precache-manifest.8522b9d473c237a909ea949a894ddcaa.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "./static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "269315a7166a061e04ac",
+    "url": "./static/css/main.012f8058.chunk.css"
+  },
+  {
+    "revision": "a5080d9e55b71a50ecd7b7d7ad022754",
+    "url": "./static/media/IRANSansWeb.a5080d9e.ttf"
+  },
+  {
+    "revision": "fa3c7bb915f38d28b8e4",
+    "url": "./static/js/1.fa3c7bb9.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "./static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "./static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "./static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "269315a7166a061e04ac",
+    "url": "./static/js/main.269315a7.chunk.js"
+  },
+  {
+    "revision": "ae8767ca8ecf4b090af58eff5dd6d3a7",
+    "url": "./static/media/Framework7Icons-Regular.ae8767ca.woff2"
+  },
+  {
+    "revision": "c0087d4d5ddd32ec52f1859b42956db6",
+    "url": "./static/media/Framework7Icons-Regular.c0087d4d.eot"
+  },
+  {
+    "revision": "4348368ab857cb57b18e1a87ba8ff284",
+    "url": "./static/media/Framework7Icons-Regular.4348368a.ttf"
+  },
+  {
+    "revision": "2e9a0313ed02aed1e206340feb36ec5c",
+    "url": "./static/media/Framework7Icons-Regular.2e9a0313.woff"
+  },
+  {
+    "revision": "07e13ce12b31374a49045aa55af2c0ad",
+    "url": "./static/media/IRANSansWeb.07e13ce1.eot"
+  },
+  {
+    "revision": "d4bb655d07591559e417030519d6fa20",
+    "url": "./static/media/IRANSansWeb.d4bb655d.woff"
+  },
+  {
+    "revision": "fa3c7bb915f38d28b8e4",
+    "url": "./static/css/1.0c3a0558.chunk.css"
+  },
+  {
+    "revision": "31df882c754d14ae5f327e34caf37dba",
+    "url": "./index.html"
+  }
+];
